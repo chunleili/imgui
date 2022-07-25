a9ca9236295cb80049b5c8d2234f27acd18f5943
chunleili <li_cl@foxmail.com>
2022-07-25 22:45:52 +0800

    play main.cpp

--- examples/example_glfw_opengl3/main.cpp
+++ examples/example_glfw_opengl3/main.cpp
@@ -7,6 +7,7 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
+#include <iostream>
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
@@ -92,10 +93,12 @@ int main(int, char**)
     //IM_ASSERT(font != NULL);
 
     // Our state
-    bool show_demo_window = true;
+    bool show_demo_window = false;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    bool flag = false;
+    float f = 0.0;
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
@@ -117,25 +120,80 @@ int main(int, char**)
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
-            static float f = 0.0f;
+            ImGui::Begin("New Window!");
+            ImGui::Text("A new text");
+
+            ImGui::Checkbox("Checkbox", &flag);
+            //std::cout << "flag=" << flag << std::endl;
+            ImGui::Text("flag = %s", flag?"true":"false");
+
+            ImGui::SliderFloat("SliderFloat", &f, -1.0f, 2.0f);
+            //std::cout << "f=" << f << std::endl;
+            ImGui::Text("f = %f", f);
+            ImGui::ColorEdit3("theColor", (float*)&clear_color);
+
+            static bool botton;
             static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            if (ImGui::Button("Button"))
+            {
+                botton = !botton;
                 counter++;
+            }
             ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::Text("button is %s pressed", botton ? "" : "not");
+            ImGui::Text("button is pressed %d times", counter);
 
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+
+            ImVec2 MousePos = io.MousePos;
+            ImGui::Text("io.MousePos: (%.1f, %.1f)", MousePos.x, MousePos.y);
+            ImGui::Text("io.MousePos: (%.1f, %.1f)", MousePos.x, MousePos.y);
+
+            if (ImGui::Button("show the demo"))
+            {
+                show_demo_window = !show_demo_window;
+            }
+
+            static int theKey;
+
+            struct funcs { static bool IsLegacyNativeDupe(ImGuiKey key) { return key < 512 && ImGui::GetIO().KeyMap[key] != -1; } }; // Hide Native<>ImGuiKey duplicates when both exists in the array
+            const ImGuiKey key_first = 0;
+            ImGui::Text("Keys pressed:");
+            for (ImGuiKey key = key_first; key < ImGuiKey_COUNT; key++)
+            {
+                if (funcs::IsLegacyNativeDupe(key)) continue;
+                if (ImGui::IsKeyPressed(key))
+                {
+                    ImGui::SameLine();
+                    ImGui::Text("\"%s\" %d", ImGui::GetKeyName(key), key);
+                    theKey = key;
+                }
+            }
+
+            ImGui::Text("the key is: %s, key ID is %d", ImGui::GetKeyName(theKey), theKey);
+
             ImGui::End();
+
+
+             //static float f = 0.0f;
+             //static int counter = 0;
+
+             //ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            // ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            // ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            // ImGui::Checkbox("Another Window", &show_another_window);
+
+            // ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            // ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            // if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            //     counter++;
+            // ImGui::SameLine();
+            // ImGui::Text("counter = %d", counter);
+
+            // ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            // ImGui::End();
         }
 
         // 3. Show another simple window.
